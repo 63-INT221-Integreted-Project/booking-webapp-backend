b2e1992f195e5aa5fa45270e24156b201b7e2f47
Thiraphat <few.thiraphat@gmail.com>
2022-04-21 14:47:20 +0700

    first commit

---
+++ src/main/java/sit/int221/bookingproj/BookingprojApplication.java
@@ -0,0 +1,13 @@
+package sit.int221.bookingproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sit/int221/bookingproj/BookingprojApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int221.bookingproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
