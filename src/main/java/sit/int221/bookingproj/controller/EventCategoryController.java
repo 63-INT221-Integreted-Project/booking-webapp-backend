a8da32ac9762edb03264a3ebd99c68443c237700
AloneCoding <jom.karunpat@alonecoding.com>
2022-04-23 14:47:24 +0700

    Chagne endpoint name

--- src/main/java/sit/int221/bookingproj/controller/EventCategoryController.java
+++ src/main/java/sit/int221/bookingproj/controller/EventCategoryController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/eventcategories")
+@RequestMapping("/event-categories")
 public class EventCategoryController {
     @Autowired
     public EventCategoryRepository eventCategoryRepository;
