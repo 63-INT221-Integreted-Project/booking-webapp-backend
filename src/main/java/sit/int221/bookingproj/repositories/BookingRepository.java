a0c952f17ce8ef9c0ac0b19b7d3d1cb499a27fc0
Thiraphat <few.thiraphat@gmail.com>
2022-04-26 21:07:20 +0700

    Delete Booking Entity, Controller, Repository After Rechive Rollback Version

--- src/main/java/sit/int221/bookingproj/repositories/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package sit.int221.bookingproj.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int221.bookingproj.entities.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, String> {
-}
