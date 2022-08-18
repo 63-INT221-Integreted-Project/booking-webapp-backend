def60032e950213010b73c33cdedfc96cf5d67da
Thiraphat <few.thiraphat@gmail.com>
2022-08-19 02:51:13 +0700

    add repository

---
+++ src/main/java/sit/int221/bookingproj/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package sit.int221.bookingproj.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sit.int221.bookingproj.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findAllByName(String name);
+    public User findAllByEmail(String email);
+}
