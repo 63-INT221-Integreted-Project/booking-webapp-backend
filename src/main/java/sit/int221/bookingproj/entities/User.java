43db27ebf3942784e15db5b8a7c156c78a435b45
Thiraphat <few.thiraphat@gmail.com>
2022-08-19 02:50:39 +0700

    add entity

---
+++ src/main/java/sit/int221/bookingproj/entities/User.java
@@ -0,0 +1,89 @@
+package sit.int221.bookingproj.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId", nullable = false)
+    private Integer userId;
+
+    @Column(name = "name", nullable = false)
+    @Size(max = 100, message = "length exceeded the size")
+    private String name;
+
+    @Column(name = "email" , nullable = false)
+    @Size(max = 50, message = "length exceeded the size")
+    private String email;
+
+    @Column(name = "role", nullable = false)
+    @Size(max = 50, message = "length exceeded the size")
+    private String role;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name="createdOn")
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name="updatedOn")
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public  LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn (LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+}
