b44b0cdafebaeff481de9a7f08cb07f61e237432
Thiraphat <few.thiraphat@gmail.com>
2022-08-24 23:17:35 +0700

    match services

---
+++ src/main/java/sit/int221/bookingproj/controller/MatchController.java
@@ -0,0 +1,26 @@
+package sit.int221.bookingproj.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import sit.int221.bookingproj.dtos.UserActionDto;
+import sit.int221.bookingproj.dtos.UserLoginDto;
+import sit.int221.bookingproj.entities.User;
+import sit.int221.bookingproj.exception.EmailUserNotFoundException;
+import sit.int221.bookingproj.exception.PasswordUserNotMatchException;
+import sit.int221.bookingproj.repositories.UserRepository;
+import sit.int221.bookingproj.services.MatchService;
+
+@RestController
+@RequestMapping("/api/match")
+public class MatchController {
+
+    @Autowired
+    public MatchService matchService;
+
+    @PostMapping(name = "")
+    @ResponseStatus(code = HttpStatus.OK)
+    public HttpStatus matchPassword(@RequestBody UserLoginDto userLoginDto) throws PasswordUserNotMatchException, EmailUserNotFoundException {
+        return matchService.matchUsernameAndPassword(userLoginDto);
+    }
+}
---
+++ src/main/java/sit/int221/bookingproj/services/MatchService.java
@@ -0,0 +1,49 @@
+package sit.int221.bookingproj.services;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import sit.int221.bookingproj.dtos.UserActionDto;
+import sit.int221.bookingproj.dtos.UserLoginDto;
+import sit.int221.bookingproj.entities.User;
+import sit.int221.bookingproj.exception.EmailUserNotFoundException;
+import sit.int221.bookingproj.exception.PasswordUserNotMatchException;
+import sit.int221.bookingproj.repositories.UserRepository;
+
+@Service
+public class MatchService {
+    @ExceptionHandler(EmailUserNotFoundException.class)
+    public void handleEmailUserNotFound(){}
+
+    @ExceptionHandler(PasswordUserNotMatchException.class)
+    public void handleEmailPasswordUserNotMatch(){}
+
+    @Autowired
+    public UserRepository userRepository;
+    public HttpStatus matchUsernameAndPassword(UserLoginDto userLoginDto) throws EmailUserNotFoundException, PasswordUserNotMatchException {
+        Argon2 argon2 = Argon2Factory.create(
+                Argon2Factory.Argon2Types.ARGON2id,
+                16,
+                16);
+        User user = new User();
+        userLoginDto.setEmail(userLoginDto.getEmail().trim());
+        userLoginDto.setPassword(userLoginDto.getPassword().trim());
+        user = userRepository.findAllByEmail(userLoginDto.getEmail());
+        if(user != null){
+            if(argon2.verify(user.getPassword() , userLoginDto.getPassword())){
+                return HttpStatus.OK;
+            }
+            else{
+                throw new PasswordUserNotMatchException("This password is not match in system");
+            }
+        }
+        else{
+            throw new EmailUserNotFoundException("This Email is Not Found");
+        }
+    }
+
+}
