e457fe0efec879b66c3f525e4c9fada3f6ac4230
Thiraphat <few.thiraphat@gmail.com>
2022-08-24 23:17:11 +0700

    handle with dto

--- src/main/java/sit/int221/bookingproj/dtos/UserLoginDto.java
+++ src/main/java/sit/int221/bookingproj/dtos/UserLoginDto.java
@@ -1,27 +1,22 @@
 package sit.int221.bookingproj.dtos;
 
 import lombok.Data;
-import org.springframework.lang.Nullable;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
-public class UserActionDto {
-    private Integer userId;
-    @NotBlank(message = "can not blank")
-    @Size(max = 100, message = "length exceeded the size")
-    private String name;
-
+@Getter
+@Setter
+public class UserLoginDto {
     @NotBlank(message = "can not blank")
     @Email(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", message = "Email is invalid")
     @Size(max = 50, message = "length exceeded the size")
-    private String email;
-
-    @Size(max = 50, message = "length exceeded the size")
-    private String role;
-
+    public String email;
     @Size(max = 14, min = 8, message = "length exceeded the size")
-    private String password;
+    public String password;
+
 }
