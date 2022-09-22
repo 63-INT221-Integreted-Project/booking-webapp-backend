a7acd336b554a0d2ab6ca3756e5719d7b834b248
Thiraphat <few.thiraphat@gmail.com>
2022-09-23 04:00:35 +0700

    handle with jwt

--- src/main/java/sit/int221/bookingproj/exception/RestExceptionHandler.java
+++ src/main/java/sit/int221/bookingproj/exception/RestExceptionHandler.java
@@ -1,11 +1,13 @@
 package sit.int221.bookingproj.exception;
 
+import org.springframework.data.rest.webmvc.support.ExceptionMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
