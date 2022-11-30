5ac5a23db5e32219a97c9075e39d59a513f92d27
Thiraphat <few.thiraphat@gmail.com>
2022-11-30 20:37:57 +0700

    remove cors origin

--- src/main/java/sit/int221/bookingproj/config/SecurityConfig.java
+++ src/main/java/sit/int221/bookingproj/config/SecurityConfig.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
-@CrossOrigin
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
