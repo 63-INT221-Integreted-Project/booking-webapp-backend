2c8e2d26b4a93bfa977109379ae36dee90787d0f
Thiraphat <few.thiraphat@gmail.com>
2022-12-19 20:52:45 +0700

    delete comment

--- src/main/java/sit/int221/bookingproj/config/WebConfig.java
+++ src/main/java/sit/int221/bookingproj/config/WebConfig.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
